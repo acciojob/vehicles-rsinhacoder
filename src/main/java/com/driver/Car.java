4ecef453d41a078b470205702d06cd4120e8b4c6
Ritik Sinha <84030322+rsinhacoder@users.noreply.github.com>
2023-01-21 23:19:27 +0530

    Solution

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,17 +9,83 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
+    public int getWheels() {
+        return this.wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getDoors() {
+        return this.doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getGears() {
+        return this.gears;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public boolean isManual() {
+        return this.isManual;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+
+    public int getCurrentGear() {
+        return this.currentGear;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
+
+    public int getSeats() {
+        return this.seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+
+
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        super(name);
+        this.wheels = wheels;
+        this.type = type;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.seats = seats;
+        this.currentGear = 1;
         //Hint: Car extends Vehicle
     }
 
     public void changeGear(int newGear){
-
+         currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+         this.move(newSpeed,newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
